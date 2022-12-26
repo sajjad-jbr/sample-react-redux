cbf7ff8094198632f36729dc7501c4d963067f6d
sajjad-jbr <job.sajjad.jbr@gmail.com>
2022-12-26 10:53:30 +0330

    fix first app redux

--- src/App.js
+++ src/App.js
@@ -1,48 +1,11 @@
-import React, {useState} from 'react';
-import store from './FirstTest/store'
+import FirstAppRedux from "./FirstTest/FirstAppRedux";
 
 function App(props) {
 
-    const unsubscribe = store.subscribe(()=>{
-        console.log("sajjad: ", {store, getState: store.getState()});
-    })
+    return(
+        <FirstAppRedux/>
+    )
 
-    const [listBug, setListBug] = useState(store.getState());
-    return (
-        <div>
-            hello world!!!
-            <button onClick={() => {
-                store.dispatch({
-                    type: "ADD_BUG",
-                    payload: {
-                        description: "bug 1"
-                    }
-                })
-                setListBug(store.getState())
-            }}>
-                add bug
-            </button>
-            <button onClick={() => {
-                store.dispatch({
-                    type: "REMOVE_BUG",
-                    payload: {
-                        ...listBug[listBug.length - 1]
-                    }
-                })
-                setListBug(store.getState())
-            }}>
-                remove bug
-            </button>
-            <br/>
-            <ul>
-                {listBug.map(bug => {
-                    return <li key={bug.id}>
-                        {`${bug.id} - ${bug.description} - ${bug.resolve}`}
-                    </li>
-                })}
-            </ul>
-        </div>
-    );
 }
 
 export default App;
---
+++ src/FirstTest/FirstAppRedux.jsx
@@ -0,0 +1,68 @@
+import React, {useState} from 'react';
+import store from "./store";
+
+let unsubscribe = null
+
+function FirstAppRedux(props) {
+
+    const handleAddBug = () => {
+        store.dispatch({
+            type: "ADD_BUG",
+            payload: {
+                description: "bug 1"
+            }
+        })
+        setListBug(store.getState())
+    }
+
+    const handleRemoveBug = () => {
+        store.dispatch({
+            type: "REMOVE_BUG",
+            payload: {
+                ...listBug[listBug.length - 1]
+            }
+        })
+        setListBug(store.getState())
+    }
+
+    const handleSubscribe = () => {
+        unsubscribe = store.subscribe(() => {
+            console.log("sajjad: ", {store, getState: store.getState()});
+        })
+    }
+
+    const handleUnsubscribe = () => {
+        if (unsubscribe !== null)
+            unsubscribe()
+    }
+
+    const [listBug, setListBug] = useState(store.getState());
+    return (
+        <div>
+            simple todo bug
+            <br/>
+            <button onClick={handleAddBug}>
+                add bug
+            </button>
+            <button onClick={handleRemoveBug}>
+                remove bug
+            </button>
+            <button onClick={handleSubscribe}>
+                subscribe store
+            </button>
+            <button onClick={handleUnsubscribe}>
+                unsubscribe store
+            </button>
+            <br/>
+            <ul>
+                {listBug.map(bug => {
+                    return <li key={bug.id}>
+                        {`${bug.id} - ${bug.description} - ${bug.resolve}`}
+                    </li>
+                })}
+            </ul>
+        </div>
+    );
+}
+
+export default FirstAppRedux;
